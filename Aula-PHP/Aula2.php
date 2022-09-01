35cd4ecf81c59a5aaf22ecbae093769b1b1ff7dc
Matheus Machado de Almeida <40668494+MatheusMachado57@users.noreply.github.com>
2022-09-01 07:29:20 -0300

    att

---
+++ Aula-PHP/Aula2.php
@@ -0,0 +1,4 @@
+<?php
+//O que for rodado aqui dentro sera codigo PHP
+echo"<p> Oi, td bom?</p>";
+?>
---
+++ Aula-PHP/Aula4.php
@@ -0,0 +1,6 @@
+<?php
+//O que for rodado aqui dentro sera codigo PHP
+echo"<p> Oi, td bom?</p>";
+$cor = "vermelha";
+echo"<p> A cor $cor </p>";
+?>
